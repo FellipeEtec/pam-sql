2d8e6e0a5299e4f6c7ed05698fa7bd5a173f0a5b
FellipeEtec <fellipe.cunha@etec.sp.gov.br>
2025-03-27 16:30:14 -0300

    Update ClientesRepository.kt

--- app/src/main/java/com/example/appaula/data/repository/ClientesRepository.kt
+++ app/src/main/java/com/example/appaula/data/repository/ClientesRepository.kt
@@ -3,6 +3,8 @@ package com.example.appaula.data.repository
 import com.example.appaula.data.model.Cliente
 import kotlinx.coroutines.flow.Flow
 
+// Interface/Molde para os tipos diversos de repositórios provenientes
+// "Repositório" é uma parte intermediária que esconde a complexidade de como os dados são adquiridos, armazenados e manipulados
 interface ClientesRepository {
     fun getAllClientesStream(): Flow<List<Cliente>>
 
