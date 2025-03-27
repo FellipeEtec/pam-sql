ef640442abf2348fdbe1996a19989f85b7a4f3fe
FellipeEtec <fellipe.cunha@etec.sp.gov.br>
2025-03-27 16:05:48 -0300

    Update Cliente.kt

--- app/src/main/java/com/example/appaula/data/model/Cliente.kt
+++ app/src/main/java/com/example/appaula/data/model/Cliente.kt
@@ -3,8 +3,10 @@ package com.example.appaula.data.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+// Anotação necessária para ser interpretada como tabela/entidade
 @Entity(tableName = "clientes")
 data class Cliente (
+    // Podem ser usadas anotações para modificar os atributos
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
 
