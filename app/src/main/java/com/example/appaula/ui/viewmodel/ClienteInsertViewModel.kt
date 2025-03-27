e61e3b07dfc6bb1eed5c84a300a225cfc76344c6
FellipeEtec <fellipe.cunha@etec.sp.gov.br>
2025-03-27 17:10:04 -0300

    Update ClienteInsertViewModel.kt

--- app/src/main/java/com/example/appaula/ui/viewmodel/ClienteInsertViewModel.kt
+++ app/src/main/java/com/example/appaula/ui/viewmodel/ClienteInsertViewModel.kt
@@ -8,14 +8,21 @@ import com.example.appaula.data.model.ClienteState
 import com.example.appaula.data.model.toCliente
 import com.example.appaula.data.repository.ClientesRepository
 
+// Classe que armazena as informações/States para a View ou Fragment e cuida da lógica das regras de negócio
 class ClienteInsertViewModel(private val clientesRepository: ClientesRepository) : ViewModel() {
+    // Estado armazenado pelo ViewModel
     var clienteState by mutableStateOf(ClienteState())
         private set
 
+    // Funções para a View se comunicar com os dados
+    
+    // Atualiza o estado do cliente dentro da classe
+    // Semelhante ao Setter
     fun updateUiState(clienteUiState: ClienteState) {
         clienteState = clienteUiState
     }
 
+    // Salva o estado do cliente no banco de dados através do repositório
     suspend fun saveCliente(): Boolean {
         if (!verifySaveable()) {
             return false
@@ -25,6 +32,7 @@ class ClienteInsertViewModel(private val clientesRepository: ClientesRepository)
         return true
     }
 
+    // Verifica se o estado atual segue as regras necessárias
     fun verifySaveable(): Boolean {
         return clienteState.nome !== ""
     }
