dc15ada5084950c2b7d68de0d11a565c219ff1b1
FellipeEtec <fellipe.cunha@etec.sp.gov.br>
2025-03-27 17:11:32 -0300

    Update EmpresaApplication.kt

--- app/src/main/java/com/example/appaula/EmpresaApplication.kt
+++ app/src/main/java/com/example/appaula/EmpresaApplication.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import com.example.appaula.data.AppContainer
 import com.example.appaula.data.AppDataContainer
 
+// Classe principal da aplicação
+// Precisa estar definida no AndroidManifest.xml
+// android:name=".EmpresaApplication"
 class EmpresaApplication: Application() {
     lateinit var container: AppContainer
 
