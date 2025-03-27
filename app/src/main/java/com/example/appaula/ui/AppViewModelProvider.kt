5dee191c46f80c5a90a90708f4cf8906ad43dd94
FellipeEtec <fellipe.cunha@etec.sp.gov.br>
2025-03-27 17:17:04 -0300

    Update AppViewModelProvider.kt

--- app/src/main/java/com/example/appaula/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/appaula/ui/AppViewModelProvider.kt
@@ -7,6 +7,9 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.appaula.EmpresaApplication
 import com.example.appaula.ui.viewmodel.ClienteInsertViewModel
 
+// Provider/Provedor de ViewModels para a aplicação através de Factory
+// Factory é padronizar a inicialização de uma ou mais classes
+// Ajuda no controle de dependencias ao centralizar as inicializações
 object AppViewModelProvider {
     val Factory = viewModelFactory {
         initializer {
@@ -15,5 +18,6 @@ object AppViewModelProvider {
     }
 }
 
+// Função para instanciar a classe da aplicação
 fun CreationExtras.empresaApplication(): EmpresaApplication =
     (this[AndroidViewModelFactory.APPLICATION_KEY] as EmpresaApplication)
