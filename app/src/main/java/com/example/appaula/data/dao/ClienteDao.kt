ab8bdb97110c8555651ddf20c9e084e649d8b35a
FellipeEtec <fellipe.cunha@etec.sp.gov.br>
2025-03-27 16:41:41 -0300

    Update ClienteDao.kt

--- app/src/main/java/com/example/appaula/data/dao/ClienteDao.kt
+++ app/src/main/java/com/example/appaula/data/dao/ClienteDao.kt
@@ -10,6 +10,10 @@ import androidx.room.Upsert
 import com.example.appaula.data.model.Cliente
 import kotlinx.coroutines.flow.Flow
 
+// Data Access Object (DAO) vulgo objeto feito para facilitar o acesso aos dados
+// Parte intermediária que, dependendo da implementação, reduz ou nulifica o uso de SQL
+// Abstrai desde poucos e simples até inúmeros e complexos códigos
+// Por ser um padrão, pode ser usado tanto em bancos de dados SQL quanto em NoSQL
 @Dao
 interface ClienteDao {
     // Unificação de Insert e Update (upsert)
